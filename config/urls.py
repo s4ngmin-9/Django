9181d40fdc1550baefe3a934a8140c7523eabf50
s4ngmin-9 <s4ngmin9@naver.com>
2025-09-16 16:55:30 +0900

    ✨feat: chapter_03 과제 업로드

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,14 @@
-"""
-URL configuration for config project.
-...
-"""
 from django.contrib import admin
 from django.urls import path, include
+from todo.views import todo_list, todo_info
+from users import views as user_views
 
 urlpatterns = [
+    path('todo/', todo_list, name='todo_list'),
+    path('todo/<int:todo_id>/', todo_info, name='todo_info'),
     path('admin/', admin.site.urls),
-    path('todo/', include('todo.urls')), # todo 앱의 urls.py를 포함시킵니다.
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', user_views.login, name='login'),
+    path('accounts/signup/', user_views.sign_up, name='signup')
 ]
+
